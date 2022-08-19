c3b559e0ec2081e283fe2cf57429f3d55d109296
SteezieJ <s.joubert@zoho.com>
2022-08-19 12:30:06 +0200

    Initial Commit

---
+++ SIGNSlate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SIGNSlate
+//
+//  Created by Stephanie Joubert on 19/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
