052183d6ab660baafa8fae0541ddba8ced3ca76f
SteezieJ <s.joubert@zoho.com>
2022-08-21 21:42:08 +0200

    Working facial tracking

---
+++ SIGNSlate/Design Files/MenuViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MenuViewController.swift
+//  SIGNSlate
+//
+//  Created by Stephanie Joubert on 21/08/2022.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        title = "Welcome screen"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
